ebeda13c27fb25c0033412df594fcd6510b0f50e
eddy_goott <kthhyung@gmail.com>
2021-04-30 16:19:46 +0900

    5장까지 연습문제

--- chap04/src/Exercise/Exercise05.java
+++ chap04/src/Exercise/Exercise05.java
@@ -2,11 +2,17 @@ package Exercise;
 //5. 중첩 for 문을 이용하여 방정식 4x + 5y = 60의 모든 해를 구해서 (x,y)의 형태로 출력해보세요.
 //단, x와 y는 10이하의 자연수입니다.
 public class Exercise05 {
-	public static void main(String[] args) {
-		int x;
-		int y;
-		for (x)// x는 4씩 늘어나고 y는 5씩 늘어나게 하고, 그 결과값이 60이 되는 x와 y깁찾기.
-		
-		System.println("x가 "+ x +"이고, y가" + y +"인 경우");
-	}
+    public static void main(String[] args) {
+        int x;
+        int y;
+
+        for (x = 1; x<= 10 ; x++) {
+            for (y = 1; y<=10 ; y++) {
+                if ((4*x + 5*y) == 60) // x는 4씩 늘어나고 y는 5씩 늘어나게 하고, 그 결과값이 60이 되는 x와 y깁찾기.
+                    System.out.println("("+ x +"," + y +")");
+
+            }
+        }
+
+    }
 }
